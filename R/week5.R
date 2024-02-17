3347992d3c33f523c7b4e3e6f5f71a2a65370c7c
Jessica Wang <wan00967@umn.edu>
2024-02-17 15:29:31 -0600

    project complete

--- R/week5.R
+++ R/week5.R
@@ -7,3 +7,18 @@ Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", escape_double 
 Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_delim("../data/Bparticipants.dat", delim = "\t", escape_double = FALSE, trim_ws = TRUE, col_names = c("casenum", "parnum","stimver", "datadate", paste0("q",1:10)))
 Bnotes_tbl <- read_delim("../data/Bnotes.txt", delim = "\t", escape_double = FALSE, trim_ws = TRUE)
+
+# Data cleaning
+Aclean_tbl<- Adata_tbl %>%
+  separate("qs", into = c("q1","q2","q3","q4","q5"), sep = "-") %>%
+  mutate(datadate=mdy_hms(datadate)) %>%
+  mutate(across(starts_with("q"), as.integer)) %>%
+  left_join(Anotes_tbl, by = "parnum") %>%
+  filter(is.na(notes))
+ABclean_tbl <- Bdata_tbl %>%
+  mutate(datadate=mdy_hms(datadate)) %>%
+  mutate(across(starts_with("q"), as.integer)) %>%
+  left_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes)) %>%
+  bind_rows(Aclean_tbl, .id = "lab") %>%
+  select(-notes)
